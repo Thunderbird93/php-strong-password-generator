a024759d5b8a221081291755bbb3581d589b40a3
Thunderbird93 <antircristian@gmail.com>
2022-12-02 17:14:05 +0100

    [add] funzione corretta

--- index.php
+++ index.php
@@ -1,26 +1,26 @@
 <?php
 
-$num = $_GET['numero'];
 //var_dump($num);
 
 
 function pswMind($num){
-    $psw = [];
+    $psw = '';
     $accepted = '!?&%$<>^+-*/()[]{}@#_=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    $num;
-    while (0<$num){
+    
+    while (strlen($psw)<$num){
         $psw.=$accepted[random_int(0, strlen($accepted)-1)];
     }
+    return $psw;
 }
 
 if(isset($_GET['numero'])){
-    pswMind($num);
+    echo pswMind($_GET['numero']);
 }
 
 // -- Milestone 1 --
 // 1.-----Creare un Form.
 // 2.-----Usando "GET" devo prendere ed inviare la lunghezza della  password (un numero)
-// 3.Creare una Funzione per generare una password casuale (lettere min. e maius. numeri e simboli)
+// 3.-----Creare una Funzione per generare una password casuale (lettere min. e maius. numeri e simboli)
 ?>
 
 <!DOCTYPE html>
