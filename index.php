3e498a6ea7b2fb4c84e07b7b1cb1d9c075518658
Thunderbird93 <antircristian@gmail.com>
2022-12-02 15:50:51 +0100

    [add] ad Form

--- index.php
+++ index.php
@@ -1,8 +1,26 @@
 <?php
 
 // -- Milestone 1 --
-// 1.Creare un Form
+// 1.-----Creare un Form.
 // 2.Usando "GET" devo prendere ed inviare la lunghezza della password (un numero)
 // 3.Creare una Funzione per generare una password casuale (lettere min. e maius. numeri e simboli)
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP STRONG PASSWORD GENERATOR</title>
+</head>
+<body>
+    <main>
+        <form action="">
+            <label>Quanto vuoi che sia lunga la tua password?</label>
+            <input type="number" name="numero" placeholder="Inserisci un numero">
+            <button type="submit">Crea Password</button>
+        </form>
+    </main>
+</body>
+</html>
