1d10c021cf742d4cc3b14116fa4acfdb6a99f370
Thunderbird93 <antircristian@gmail.com>
2022-12-02 15:42:18 +0100

    [init] Milestone 1

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
 
+// -- Milestone 1 --
+// 1.Creare un Form
+// 2.Usando "GET" devo prendere ed inviare la lunghezza della password (un numero)
+// 3.Creare una Funzione per generare una password casuale (lettere min. e maius. numeri e simboli)
 ?>
+
