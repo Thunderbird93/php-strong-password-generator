31408fa80da2f02fb7784b6de83a193da7abcc24
Thunderbird93 <antircristian@gmail.com>
2022-12-02 16:44:42 +0100

    [add] dove creare la password

--- index.php
+++ index.php
@@ -3,13 +3,14 @@
 $num = $_GET['numero'];
 //var_dump($num);
 
-function pswMind($num){
 
+function pswMind($num){
+    $psw = [];
 }
 
 // -- Milestone 1 --
 // 1.-----Creare un Form.
-// 2.-----Usando "GET" devo prendere ed inviare la lunghezza della password (un numero)
+// 2.-----Usando "GET" devo prendere ed inviare la lunghezza della  password (un numero)
 // 3.Creare una Funzione per generare una password casuale (lettere min. e maius. numeri e simboli)
 ?>
 
